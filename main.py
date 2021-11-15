185880e5ba95cba5b3fe5d40084a9f310cbc378b
seoyeon <ohj3300@naver.com>
2021-11-15 22:12:48 +0900

    .
    .

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import os
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online)
+    print('[알림][유미봇이 성공적으로 구동되었습니다.]')
+
+@client.event
+async def on_message(message):
+    if message.content == "!내전":
+        channel = client.get_channel(877148347939581972)
+        msg = await channel.send('@everyone \n 내전')
+        await msg.add_reaction('<:yes:909402065799872522>')
+        await msg.add_reaction('<:no:909402066001199175>')
+
+
+client.run(os.environ['token'])
